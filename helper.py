65e3bfa59e76e0d049efc72db232b5f7c2986050
TravisCossarini <traviscossarini@gmail.com>
2024-02-08 11:56:11 -0500

    Added new function to load url instead of instantiating new web driver, takes ~1 second less (66% reduction)

--- helper.py
+++ helper.py
@@ -27,6 +27,16 @@ def init_web_driver(link: str, headless: bool = HEADLESS_MODE):
     logging.info(f"Initializing Web Driver - waited {time.time() - start_time:.2f} for link {link}")
     return driver
 
+def driver_get_new_page(driver: webdriver, link: str) -> webdriver:
+    """Gets a new page for webdriver"""
+    start_time = time.time()
+    driver.get(link)
+    WebDriverWait(driver, 10).until(
+        expected_conditions.presence_of_element_located((By.TAG_NAME, "body"))
+    )
+    logging.info(f"Getting new page - waited {time.time() - start_time:.2f} for link {link}")
+    return driver
+
 def get_page_source(link: str):
     """Returns the source for a given link"""
     driver = init_web_driver(link)
