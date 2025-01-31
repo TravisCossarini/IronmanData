de314e67b114099123903d19e883963055d23a81
TravisCossarini <traviscossarini@gmail.com>
2025-01-30 23:42:49 -0800

    Updated to use api instead of selenium

--- get_competitor_lab_urls.py
+++ get_competitor_lab_urls.py
@@ -3,126 +3,73 @@ import logging
 import time
 import concurrent.futures
 import pandas as pd
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.common.exceptions import ElementClickInterceptedException, ElementNotInteractableException, StaleElementReferenceException, NoSuchElementException, TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
 import helper
+from bs4 import BeautifulSoup
+import json
 
 # CONFIG VALUES
-IRONMAN_BASE_LINK = "https://www.ironman.com"
-IRONMAN_RACES_LINK = f"{IRONMAN_BASE_LINK}/races"
-COMPETITOR_LAB_LINK = "https://labs.competitor.com/result/subevent/"
-COMPETITOR_LAB_LINK_SUFFIX= "?filter=%7B%7D&order=ASC&perPage=50&sort=FinishRankOverall&page="
+COMPETITOR_LAB_EVENT_LINK = "https://labs-v2.competitor.com/results/event"
+COMPETITOR_LAB_EVENT_LINK_OPEN = "https://labs-v2.competitor.com/results/event/odiv"
+# COMPETITOR_LAB_EVENT_LINK_TRICLUB = "https://labs-v2.competitor.com/clubpoints/event"
 NUM_THREADS = 4
 RACE_IDS_FILE = "Race Information.csv"
 
-def get_competitor_labs_urls(race_id: str):
-    """Gets all clab urls for a given race id by clicking through the tabs of the iframe node"""
-    logging.info(f"Getting data page for race {race_id}")
-    results_link = f"{IRONMAN_BASE_LINK}/{race_id}-results"
+def get_competitor_labs_urls(race_id: str, clab_event_id: str):
+    """Gets all clab urls for a given race id"""
+    start_time = time.time()
+    logging.info(f"Getting subevents for {race_id} with clab id {clab_event_id}")
+    race_clab_subevents = []
 
-    driver = helper.init_web_driver(results_link)
-    # Wait for cookies form to load
-    time.sleep(5)
-    try:
-        driver.find_element(By.ID, "onetrust-reject-all-handler").click()
-    except NoSuchElementException as error:
-        logging.warning(f"No cookies found on {race_id} --- {error}")
-
-
-    race_competitor_labs_urls = []
-    current_year_url_ids = set()
-    years = set()
-
-    results_years_buttons = driver.find_elements(By.CSS_SELECTOR, ".tab-remote")
-    # results_years_buttons = driver.find_elements(By.XPATH, "//span[text()='Results']/../../../following-sibling::div/div/ul/li/span/a")
-
-    for result_year in results_years_buttons:
+    for url in [COMPETITOR_LAB_EVENT_LINK, COMPETITOR_LAB_EVENT_LINK_OPEN]:
+        subevent_response = helper.make_request(f"{url}/{clab_event_id}", headers=helper.IRONMAN_REQUEST_HEADER)
+        subevent_html = BeautifulSoup(subevent_response.text, "html.parser")
         try:
-            result_year.click()
-            time.sleep(10)
-            iframes_list = driver.find_elements(By.XPATH, f"//iframe[contains(@src, '{COMPETITOR_LAB_LINK}')]")
+            subevent_html = subevent_html.find("script", {'id': '__NEXT_DATA__'}).string
+        except AttributeError as e:
+            logging.error("ERRROR Likely blocked by IP address")
+            raise e
+        subevents = json.loads(subevent_html)
+        if("err" not in subevents):
+            subevents = subevents["props"]["pageProps"]["subevents"]
+        else:
+            logging.error(f"{race_id} failed with error {subevents['err']["name"]}: {subevents['err']["message"]} ")
+            return race_clab_subevents
 
-            for iframe in iframes_list:
-                iframe_url = iframe.get_attribute("src")
-                current_year_url_ids.add(iframe_url.split("/")[-1])
-                years.add(result_year.text if result_year.text != "" else "Blank")
+        for subevent in subevents:
+            subevent_dict = {
+                "Race Id": race_id,
+                "Name": subevent["wtc_name"],
+                "Event Date": subevent["wtc_eventdate_formatted"],
+                "Subevent Id": subevent["wtc_eventid"]
+            }
+            race_clab_subevents.append(subevent_dict)
+    logging.info(f"Got {len(race_clab_subevents)} subevents for race {race_id} {round(time.time() - start_time, 2)} seconds")
+    return race_clab_subevents
 
-            logging.info(f"Competitor labs ID retreived for {race_id} - {result_year.text}")
-        except (ElementClickInterceptedException, ElementNotInteractableException, StaleElementReferenceException, NoSuchElementException) as error:
-            logging.warning(f"Error element on {race_id} - {result_year.text} for year {result_year.text} --- {error}")
-
-    race_object = {
-        "id": race_id,
-        "competitor_lab_ids": ", ".join(list(current_year_url_ids)),
-        "years": ", ".join(list(years)) # Included for QA
-    }
-    race_competitor_labs_urls.append(race_object)
-
-    driver.quit()
-    return race_competitor_labs_urls
-
-def get_all_race_data_urls(race_ids: list):
+def get_all_clab_subevents(race_data: pd.DataFrame):
     """Gets all of the competitor lab urls for a given set of race ids"""
-    race_dimension_info = []
+    logging.info(f"Getting all subevents for {race_data.shape[0]} races")
+    clab_subevents = []
+
+    race_ids = race_data["Race Id"].to_list()
+    clab_event_ids = race_data["Competitor Lab Id"].to_list()
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
-        result_urls = list(executor.map(get_competitor_labs_urls, race_ids))
+        result_subevents = list(executor.map(get_competitor_labs_urls, race_ids, clab_event_ids))
 
-    for urls in result_urls:
-        race_dimension_info += urls
-
-    return pd.DataFrame(race_dimension_info)
-
-def fetch_competitor_labs_information(clab_id: str):
-    """Gets the supporting information such as year for each competitor labs url"""
-    results_link = f"{COMPETITOR_LAB_LINK}{clab_id}"
-    driver = helper.init_web_driver(results_link)
-    logging.info(f"Getting year from clabs for {results_link}")
-
-    WebDriverWait(driver, 10).until(
-        expected_conditions.presence_of_element_located((By.XPATH, "//*[@id='react-admin-title']/span/span/span"))
-    )
-    return driver.find_element(By.XPATH, "//*[@id='react-admin-title']/span/span/span").text.split(" ")[0]
-
-def compare_race_data(df1: pd.DataFrame, df2: pd.DataFrame):
-    """Compares the competitor lab urls gathered from a race"""
-    # Ugly function, I know
-    formatted_output_list = []
-    for _, row1 in df1.iterrows():
-        for _, row2 in df2.iterrows():
-            if row1["id"] == row2["id"]:
-                if row1["competitor_lab_ids"] != row2["competitor_lab_ids"] and row1["years"] != row2["years"]:
-                    logging.warn(f"Race {IRONMAN_BASE_LINK}/{row1['id']}-results has no data or there was an error extracting the urls")
-                else:
-                    for clab_id in row1["competitor_lab_ids"].split(", "):
-                        clabs_info = {
-                            "Race Id": row1["id"],
-                            "Competitor Labs ID": clab_id,
-                            "Year": fetch_competitor_labs_information(clab_id)
-                        }
-                        formatted_output_list.append(clabs_info)
-    
-    return pd.DataFrame(formatted_output_list)
+    for subevents in result_subevents:
+        clab_subevents += subevents
+    return pd.DataFrame(clab_subevents).drop_duplicates()
 
 def main():
     """Main function for module"""
     helper.set_logger()
     logging.info("Beginning extraction of competitor labs urls")
     
-    race_ids = pd.read_csv("Race Information.csv")["Race Id"].to_list()
+    race_data = pd.read_csv("Race Information.csv")
     # Do it twice to make sure that we got them all. There is latency on the website which sometimes causes errors.
-    clab_urls_1 = get_all_race_data_urls(race_ids)
-    clab_urls_2 = get_all_race_data_urls(race_ids)
-
-    try:
-        final_data = compare_race_data(clab_urls_1, clab_urls_2)
-        final_data.to_csv("Competitor Labs URLs.csv", index=False)
-    except TimeoutException as error:
-        logging.error(f"Error getting year for each URL: {error}")
-        clab_urls_1.to_csv("clab_url_1.csv", index=False)
-        clab_urls_2.to_csv("clab_url_2.csv", index=False)
+    clab_subevents = get_all_clab_subevents(race_data)
+    clab_subevents.to_csv("Competitor Labs URLs.csv", index=False)
 
 if __name__ == "__main__":
     start_time = time.time()
