d527de11568d0fdb09093674460b9665e868fa37
TravisCossarini <traviscossarini@gmail.com>
2025-02-06 13:56:18 -0800

    Updated documentation

--- get_clab_data.py
+++ get_clab_data.py
@@ -1,140 +1,112 @@
 """Gets the race data from all competitor labs urls and stores it as html files"""
 import logging
 import time
-import os
 import pandas as pd
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, TimeoutException, ElementClickInterceptedException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium import webdriver
 import helper
+import json
+import concurrent.futures
 
 # CONFIG VALUES
-IRONMAN_BASE_LINK = "https://www.ironman.com"
-IRONMAN_RACES_LINK = f"{IRONMAN_BASE_LINK}/races"
-COMPETITOR_LAB_LINK = "https://labs.competitor.com/result/subevent/"
-COMPETITOR_LAB_LINK_SUFFIX= "?filter=%7B%7D&order=ASC&perPage=50&sort=FinishRankOverall&page="
-NUM_THREADS = 4
-RETRY_COUNT = 3
+COMPETITOR_LAB_API = "https://api.competitor.com/public/result/subevent"
+PARTICIPANTS_PER_PAGE = 200
+COMPETITOR_LAB_API_SUFFIX= f"?%24limit={PARTICIPANTS_PER_PAGE}&%24skip="
+SUBSCRIPTION_KEY = "73f0c80357b249f189c34dc78ac7b118"
+NUM_THREADS = 6
 
 def get_clab_data(clab_data_id: str):
-    """Gets the data from all pages for a given clab event"""
-    data_url_first_page = f"{COMPETITOR_LAB_LINK}{clab_data_id}{COMPETITOR_LAB_LINK_SUFFIX}1"
-    driver = helper.init_web_driver(data_url_first_page)
-    time.sleep(2)
+    """Gets the data from all pages for a given clab subevent
+    
+    returns: List of dictionaries, one dict per participant
+    """
+    # Add subscription key to header
+    helper.IRONMAN_REQUEST_HEADER.update({"wtc_priv_key": SUBSCRIPTION_KEY})
+    
+    # Make first request to determine total number of participants
+    subevent_data = helper.make_request(f"{COMPETITOR_LAB_API}/{clab_data_id}/{COMPETITOR_LAB_API_SUFFIX}0", headers=helper.IRONMAN_REQUEST_HEADER)
     try:
-        num_pages = int(driver.find_elements(By.XPATH, "//ul[contains(@class, 'MuiPagination-ul')]/li/button")[-2].text)
-    except IndexError:
-        # If there is only one page, handle error
-        num_pages = 1
-    logging.info(f"Extracing data for {clab_data_id} with {num_pages} pages")
-
-    data_page_urls = [f"{COMPETITOR_LAB_LINK}{clab_data_id}{COMPETITOR_LAB_LINK_SUFFIX}{page_num}" for page_num in range(1, num_pages+1)]
-
+        total_participants = json.loads(subevent_data.text)['total']
+    except KeyError as e:
+        logging.error(subevent_data, subevent_data.text)
+        raise e
+    
+    logging.info(f"Extracing data for {clab_data_id} with {total_participants} participants")
+    
     competitor_data_list = []
-    for url in data_page_urls:
-        competitor_data_list += clabs_page_data_extraction_handler(driver, url)
 
-    save_str_list_to_file(clab_data_id, competitor_data_list)
-    driver.quit()
+    # Iterate through pages
+    for index in range(0, total_participants, PARTICIPANTS_PER_PAGE):
+        subevent_data = helper.make_request(f"{COMPETITOR_LAB_API}/{clab_data_id}/{COMPETITOR_LAB_API_SUFFIX}{index}", headers=helper.IRONMAN_REQUEST_HEADER)
+        subevent_json = json.loads(subevent_data.text)
+        for participant in subevent_json["data"]:
+            result = {
+                "Clab Event Id": clab_data_id,
+                "Contact":  participant['Contact']["FullName"],
+                "Gender":  participant['Contact']["Gender"],
+                "Contact Id": participant["ContactId"],
+                "Age Group": participant["AgeGroup"],
+                "Country": participant["CountryISO2"],
+                "Country Code": participant["CountryRepresentingISONumeric"],
+                "Bib Number": participant["BibNumber"],
+                "Event Status": participant["EventStatus"],
+                "Run Time": participant["RunTimeConverted"],
+                "Swim Time": participant["SwimTimeConverted"],
+                "Bike Time": participant["BikeTimeConverted"],
+                "Transition1 Time": participant["Transition1TimeConverted"],
+                "Transition2 Time": participant["Transition2TimeConverted"],
+                "Finish Time": participant["FinishTimeConverted"],
+                "Finish Rank Group": participant["FinishRankGroup"],
+                "Finish Rank Gender": participant["FinishRankGender"],
+                "Finish Rank Overall": participant["FinishRankOverall"],
+                "Swim Rank Group": participant["SwimRankGroup"],
+                "Swim Rank Gender": participant["SwimRankGender"],
+                "Swim Rank Overall": participant["SwimRankOverall"],
+                "Bike Rank Group": participant["BikeRankGroup"],
+                "Bike Rank Gender": participant["BikeRankGender"],
+                "Bike Rank Overall": participant["BikeRankOverall"],
+                "Run Rank Group": participant["RunRankGroup"],
+                "Run Rank Gender": participant["RunRankGender"],
+                "Run Rank Overall": participant["RunRankOverall"],
+                "Rank Points": participant["RankPoints"],
+                "Sync Date": participant["SyncDate"],
+                "Result Id": participant["ResultId"]
+            }
+            competitor_data_list.append(result)
 
-def clabs_page_data_extraction_handler(driver: webdriver, data_url: str) -> list:
-    """Function to handle extraction failures and facilitate retries for clab data"""
-    current_page = data_url.split("=")[-1]
-    clab_data_id = data_url[len(COMPETITOR_LAB_LINK): len(COMPETITOR_LAB_LINK)+36]
-    logging.info(f"Extracting data for page {current_page}")
-    driver = helper.driver_get_new_page(driver, data_url)
-    error_count = 0
-    complete_flag = False
+    logging.info(f"Extraced data for {clab_data_id} for {len(competitor_data_list)} participants")
+    return competitor_data_list
 
-    try:
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.TAG_NAME, "tr"))
-        )
-    except TimeoutException:
-        logging.error(f"Data page {current_page}, caused a timeout when waiting for data to load. Skipping race and page")
-        return []
+def clab_extraction_handler(clab_subevent_id: str):
+    """Handles the data extraction for a single subevent and saves data to csv"""
+    start_time_main = time.time()
+    logging.info(f"Beginning {clab_subevent_id} extraction")
+    results_list = []
 
-    while not complete_flag:
-        try:
-            page_values = clabs_extract_data_from_page(driver, current_page, clab_data_id)
-            complete_flag = True
-            return page_values
-        except (StaleElementReferenceException, NoSuchElementException, ElementClickInterceptedException) as error:
-            if error_count == RETRY_COUNT:
-                logging.error(f"Retried page {data_url} {error_count} times, raising error: {error}")
-                raise error
-            
-            logging.error(f"Retrying page {current_page}, retry count {error_count}: {error}")
-            # Retry entire page
-            driver.refresh()
-            time.sleep(5)
-            error_count += 1
+    # Generates a list of dictionaries
+    results_list += get_clab_data(clab_subevent_id)
 
-def clabs_extract_data_from_page(driver: webdriver, current_page: int, race_data_id: str) -> list:
-    """Extracts all data from a clab given page"""
-    competitor_html_list = []
-    start_time = time.time()
-
-    table_rows = driver.find_elements(By.XPATH, "//tbody/tr")
-
-    for idx, table_row in enumerate(table_rows):
-        table_row.click()
-        # Wait until data has loaded fully
-        try:
-            WebDriverWait(driver, 5).until(
-                EC.presence_of_element_located((By.ID, "swimDetails"))
-                and EC.presence_of_element_located((By.ID, "bikeDetails"))
-                and EC.presence_of_element_located((By.ID, "runDetails"))
-                and EC.presence_of_element_located((By.XPATH, "//div[contains(@class, 'text') and contains(@class, 'countryFlag')]/img"))
-            )
-        except TimeoutException:
-            logging.warning(f"Found row with no country on row {idx+1} on page {current_page}, timed out")
-
-        expanded_table_row = driver.find_element(By.CLASS_NAME, "RaDatagrid-expandedPanel")
-        row_html = table_row.get_attribute("outerHTML")
-        data_html = expanded_table_row.get_attribute("outerHTML")
-
-        # Click the row again to close
-        table_row.click()
-        WebDriverWait(driver, 5).until_not(
-            EC.presence_of_element_located((By.ID, "swimDetails"))
-        )
-
-        # Add div for easier parsing
-        total_html_row = f"<div class='extraction_dummy'>{row_html}{data_html}</div>"
-        competitor_html_list.append(total_html_row)
-
-    logging.info(f"Total execution time for race {race_data_id} and page {current_page} is {time.time() - start_time:.2f}")
-    return competitor_html_list
-
-def save_str_list_to_file(race_id: str, list_of_str: list):
-    """
-    Saves a list of strings to a text file
-    """
-    with open(f"Race Data HTML/{race_id}.html", "w", encoding="utf-8") as file:
-        file.write("\n".join(list_of_str))
+    logging.info(f"Total extraction time for race {clab_subevent_id} is {time.time() - start_time_main:.2f} seconds")
+    pd.DataFrame(results_list).to_csv("Participant Data.csv", mode="a", header=False, index=False)
 
 def main():
     """Main function for module"""
-    helper.set_logger("clab_HTML_extraction")
+    helper.set_logger("get_clab_data")
     logging.info("Beginning extraction of competitor labs data")
 
+    # Read data
     clab_data = pd.read_csv("Competitor Labs URLs.csv")
-    existing_files = [file[:-5] for file in os.listdir(r"./Race Data HTML") if file.endswith(".html")]
-    # Can also add one to check for csv
+    complete_races = pd.read_csv("Participant Data.csv")["Clab Event Id"].unique()
+    pending_races = []
 
+    # Get all pending races
     for _, row in clab_data.iterrows():
-        clab_id = row["Competitor Labs ID"]
+        clab_subevent_id = row["Subevent Id"]
+        if(clab_subevent_id not in complete_races):
+            pending_races.append(clab_subevent_id)
+        else:   
+            logging.info(f"Already completed {clab_subevent_id}, skipping extraction")
 
-        if clab_id not in existing_files:
-            logging.info(f"Beginning {row['Race Id']} for year {row['Year']} and id {clab_id}")
-            start_time_main = time.time()
-            get_clab_data(clab_id)
-            logging.info(f"Total extraction time for race {clab_id} is {time.time() - start_time_main:.2f}")
-
-    return
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+        executor.map(clab_extraction_handler, pending_races)
 
 if __name__ == "__main__":
     main()
