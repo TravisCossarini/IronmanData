a6ba7995facc04f0ed10cac38a5687df696006ae
TravisCossarini <traviscossarini@gmail.com>
2025-02-06 13:30:32 -0800

    Removed selenium functions as they are no longer needed

--- helper.py
+++ helper.py
@@ -31,38 +31,6 @@ IRONMAN_REQUEST_HEADER = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
 }
 
-def init_web_driver(link: str = "https://www.google.com/", headless: bool = HEADLESS_MODE):
-    """Initializes a web driver for a given URL"""
-    options = Options()
-    if headless:
-        options.add_argument("--headless=new")
-    start_time = time.time()
-    driver = webdriver.Chrome(options=options)
-    driver.get(link)
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.TAG_NAME, "body"))
-    )
-
-    logging.info(f"Initializing Web Driver - waited {time.time() - start_time:.2f} for link {link}")
-    return driver
-
-def driver_get_new_page(driver: webdriver, link: str) -> webdriver:
-    """Gets a new page for webdriver"""
-    start_time = time.time()
-    driver.get(link)
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.TAG_NAME, "body"))
-    )
-    logging.info(f"Getting new page - waited {time.time() - start_time:.2f} for link {link}")
-    return driver
-
-def get_page_source(link: str):
-    """Returns the source for a given link"""
-    driver = init_web_driver(link)
-    source = driver.page_source
-    driver.quit()
-    return BeautifulSoup(source, "html.parser")
-
 def get_formatted_time():
     """Returns formatted time for logging purposes"""
     current_time = datetime.now()
